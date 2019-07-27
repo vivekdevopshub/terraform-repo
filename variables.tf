a4c78430456755711512b32381ec298d6aea0780
vivek <kumarvivek.vik@gmail.com>
2019-07-27 19:46:49 +0530

    commit of terraform code

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "count" {
+  default = 1
+}
+
+variable "key_name" {
+  description = "Private key name to use with instance"
+  default     = "terraform"
+}
+
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "Base AMI to launch the instances"
+
+  # AWS base Windows 2019 image
+  default = "ami-028b3bf1662e6082f"
+}
