253778701c79e5f7ad797c06a260708bdc0fc2d7
vivekdevopshub <36101003+vivekdevopshub@users.noreply.github.com>
2020-01-24 13:32:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,51 +1,59 @@
 
-# Use AWS Terraform provider
-provider "aws" {
-  region = "ap-south-1"
-#  version = "~> 0.11
-}
-
-# Create EC2 instance
-resource "aws_instance" "default" {
-  ami                    = "ami-0217a85e28e625474"
-  count                  = "1"
-  key_name               = "terraform"
-  vpc_security_group_ids = ["${aws_security_group.default.id}"]
-  source_dest_check      = false
-  instance_type          = "t2.micro"
-
-  tags {
-    Name = "terraform-default"
-  }
-}
-
-# Create Security Group for EC2
-resource "aws_security_group" "default" {
-  name = "terraform-default-sg"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-}
-
-
-# Terraform state will be stored in S3
-terraform {
-  backend "s3" {
-    bucket = "terraform-bucket-vik"
-    key    = "terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
+# Use AWS Terraform provider 
+3 provider "aws" { 
+4   region = "ap-south-1" 
+5 #  version = "~> 0.11 
+6 } 
+7 
+ 
+8 # Create EC2 instance 
+9 resource "aws_instance" "default" { 
+10   ami                    = "${var.ami}" 
+11   count                  = "${var.count_instance}" 
+12   key_name               = "${var.key_name}" 
+13   vpc_security_group_ids = ["${aws_security_group.default.id}"] 
+14   source_dest_check      = false 
+15   instance_type          = "${var.instance_type}" 
+16 
+ 
+17   tags { 
+18     Name = "terraform-default" 
+19   } 
+20 } 
+21 
+ 
+22 # Create Security Group for EC2 
+23 resource "aws_security_group" "default" { 
+24   name = "terraform-default-sg" 
+25 
+ 
+26   ingress { 
+27     from_port   = 80 
+28     to_port     = 80 
+29     protocol    = "tcp" 
+30     cidr_blocks = ["0.0.0.0/0"] 
+31   } 
+32 
+ 
+33   ingress { 
+34     from_port   = 22 
+35     to_port     = 22 
+36     protocol    = "tcp" 
+37     cidr_blocks = ["0.0.0.0/0"] 
+38   } 
+39 
+ 
+40 } 
+41 
+ 
+42 
+ 
+43 # Terraform state will be stored in S3 
+44 terraform { 
+45   backend "s3" { 
+46     bucket = "terraform-bucket-vik" 
+47     key    = "terraform.tfstate" 
+48     region = "ap-south-1" 
+49   } 
+50 } 
 
